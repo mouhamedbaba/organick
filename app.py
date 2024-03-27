c516a425975bcc7f5174e23866943e5080530f07
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-03-27 18:39:38 +0000

    add contact and services

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ DEBUG = True
 @app.route("/")
 def home():
     return render_template("pages/home.html")
+
+@app.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+@app.route("/services")
+def services():
+    return render_template("pages/services.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("pages/404.html")
